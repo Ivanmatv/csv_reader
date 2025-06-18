e359a8b4cd7d0c9e2011ac366b83d208ea1ee232
Ivanmatv <feast0075@gmail.com>
2025-06-18 18:24:46 +0300

    Сделал чтение csv файла

---
+++ main.py
@@ -0,0 +1,18 @@
+import csv
+import argparse
+
+
+def read_csv(file_path):
+    """Чтение CSV файла."""
+    with open(file_path, newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        return [row for row in reader]
+
+
+parser = argparse.ArgumentParser(description="Чтение CSV файла")
+parser.add_argument("file")
+args = parser.parse_args()
+
+rows = read_csv(args.file) 
+
+print(rows)
