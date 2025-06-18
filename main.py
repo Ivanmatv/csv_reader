38db59ba3750203e1db7d4f3db60ca9418d46601
Ivanmatv <feast0075@gmail.com>
2025-06-18 19:00:41 +0300

    Поменял чтение файла

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 def read_csv(file_path):
     """Чтение CSV файла."""
     with open(file_path, newline="", encoding="utf-8") as file:
-        reader = csv.DictReader(file)
+        reader = csv.reader(file)
         return [row for row in reader]
 
 
@@ -13,6 +13,6 @@ parser = argparse.ArgumentParser(description="Чтение CSV файла")
 parser.add_argument("file")
 args = parser.parse_args()
 
-rows = read_csv(args.file) 
+rows = read_csv(args.file)
 
 print(rows)
